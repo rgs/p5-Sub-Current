5938cf74c957123c28e2d570e1170f64bccf32d0
Rafael Garcia-Suarez <rgs@consttype.org>
2017-01-30 17:01:17 +0100

    Update documentation, add deprecation notice
    and put a link to the new github repository

--- lib/Sub/Current.pm
+++ lib/Sub/Current.pm
@@ -37,9 +37,16 @@ this function will return undef.
 Outside of a special block (that is, at the top level of a program)
 C<ROUTINE()> will return undef as well.
 
+B<Note>: on perl 5.16.0 and later you can use the built-in C<__SUB__>
+instead, enabled by the pragma C<use feature 'current_sub'>.
+
+=head1 SOURCE
+
+A repository for the sources is at L<https://github.com/rgs/p5-Sub-Current>.
+
 =head1 COPYRIGHT
 
-(c) Copyright 2007 by Rafael Garcia-Suarez.
+(c) Copyright 2007, 2017 by Rafael Garcia-Suarez.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
